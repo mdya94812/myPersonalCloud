e0fff7181f607e3c87399c2b18a89e5d62e7b54d
mdya94812 <mdya94812@gmail.com>
2024-11-21 14:11:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ async function downloadFile(url, filename) {
 
     const link = document.createElement("a");
     link.href = blobUrl;
-    link.download = filename;
+    link.download = getSecondPart(filename);
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
